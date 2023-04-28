b783bb9b573be33b567b1e5bb5c83adae2e327b4
telesshelton <stelesu21@gmail.com>
2023-04-28 04:20:17 +0200

    nada

--- src/Node.java
+++ src/Node.java
@@ -3,6 +3,16 @@ public class Node <T> {
 
     private T elemento;
     private Node<T>proximo;
+
+
+    //gerar construtor vazio
+
+    public Node(T elemento){
+        this.elemento = elemento;
+        this.proximo = null;
+    }
+
+    public Node
     
     public T getElemento() {
         return elemento;
@@ -16,6 +26,13 @@ public class Node <T> {
     public void setProximo(Node<T> proximo) {
         this.proximo = proximo;
     }
+
+    @Override
+    public String toString() {
+        return "Node [elemento=" + elemento + ", proximo=" + proximo + "]";
+    }
+
+    
 
     
 }
